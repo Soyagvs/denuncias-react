7e1a3d2aa655dd943cb1780083b147acd9160d07
soyagvs <agustinmartinezyt@gmail.com>
2024-11-28 17:48:58 -0300

    add image 3d

--- denuncias-cbu/src/pages/Hero.jsx
+++ denuncias-cbu/src/pages/Hero.jsx
@@ -1,17 +1,21 @@
+import FaceID from "/FaceID.png"
 import { BtnHeroDenunciar } from "../ui/BtnHeroDenunciar"
 
 export const Hero = () => {
     return (
-        <main className="max-w-[870px] mx-auto h-screen">
+        <main className="max-w-[807px] mx-auto h-[900px] flex max-md:flex-col">
             <div className="flex flex-col gap-10">
-                <h1 className="text-9xl w-[870px] pt-36 leading-tight font-medium">
-                    <span className="uppercase bg-blue-500">Denuncia</span> de los <span className="uppercase underline decoration-red-500 decoration-[20px]">cbu</span>
+                <h1 className="md:text-8xl w-[508px] pt-36 leading-tight font-medium ">
+                    <span className="uppercase bg-blue-500">Denuncia</span> a los <span className="uppercase underline decoration-red-500 decoration-[20px]">cbu</span>
                 </h1>
-                <h2 className="text-xl font-thin max-w-[600px] ">
+                <h2 className="text-base max-w-[508px] text-[#191919] leading-tight">
                     Reporta actividades sospechosas asociadas a cuentas bancarias de manera rápida, segura y confidencial. Juntos podemos combatir el fraude y proteger a nuestra comunidad.
                 </h2>
-            </div>
                 <BtnHeroDenunciar />
+            </div>
+            <div className="flex justify-center items-center w-72 ml-12 mb-44">
+                <img src={FaceID} alt="FaceID invalid" />
+            </div>
         </main>
     )
 }
--- denuncias-cbu/src/ui/BtnHeroDenunciar.jsx
+++ denuncias-cbu/src/ui/BtnHeroDenunciar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export const BtnHeroDenunciar = () => {
     return (
         <Link to="/home">
-            <div className="bg-black w-48 h-10 mt-5">
+            <div className="bg-black w-48 h-10">
                 <button className="bg-blue-500 text-xl w-48 h-10 flex justify-center items-center text-white hover:translate-y-2 hover:translate-x-2 transition-all">
                     Denunciar
                 </button>
